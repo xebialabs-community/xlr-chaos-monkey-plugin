1b0da5c0433e5898e0a361bcc5295a0ec9af67cf
ndebuhr <ndebuhr@protonmail.com>
2019-06-03 15:59:47 -0400

    Expand chaos monkey tasks to include future task failures

---
+++ src/main/resources/chaosMonkey/FailTaskInRelease.py
@@ -0,0 +1,17 @@
+import random
+
+from org.slf4j import LoggerFactory
+
+logger = LoggerFactory.getLogger("chaosMonkey")
+
+tasks = [task for phase in getCurrentRelease().phases for task in phase.tasks]
+pendingStatuses = ['PLANNED', 'PENDING', 'IN_PROGRESS', 'WAITING_FOR_INPUT']
+pendingTasks = [task for task in tasks if task.status in pendingStatuses]
+
+if len(pendingTasks) > 0:
+    randomTaskInd = random.randint(0, len(pendingTasks)-1)
+    taskApi.failTask(pendingTasks[randomTaskInd].id, "Task failed by Chaos Monkey")
+    logger.warning('Chaos monkey strikes - failed task %s' % pendingTasks[randomTaskInd].id)
+else:
+    logger.warning('No pending tasks in release - failing Chaos Monkey task')
+    taskApi.failTask(getCurrentTask().id, "Task faild by Chaos Monkey")
