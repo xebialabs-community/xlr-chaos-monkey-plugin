51b2ad21528073c88a6db9407120eed8d71bc5c3
ndebuhr <ndebuhr@protonmail.com>
2019-06-03 16:49:10 -0400

    Commit changes to task precondition after the foobar change

--- src/main/resources/chaosMonkey/FailTaskInPhase.py
+++ src/main/resources/chaosMonkey/FailTaskInPhase.py
@@ -11,8 +11,10 @@ pendingTasks = [task for task in tasks if task.status in pendingStatuses]
 if len(pendingTasks) > 0:
     randomTaskInd = random.randint(0, len(pendingTasks)-1)
     pendingTasks[randomTaskInd].precondition = "foo = bar"
+    taskApi.updateTask(pendingTasks[randomTaskInd])
     logger.warn('Chaos monkey strikes - failed task %s' % pendingTasks[randomTaskInd].id)
 else:
     logger.warn('No pending tasks in phase - failing Chaos Monkey task')
     getCurrentTask.precondition = "foo = bar"
+    taskApi.updateTask(getCurrentTask)
     taskApi.retryTask(getCurrentTask().id, "Task failed by Chaos Monkey")
